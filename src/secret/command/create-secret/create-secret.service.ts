0d35828dea00ea0b2def3a345ffb615f6ba6bf41
Shishir Neupane <shishirs520@gmail.com>
2022-01-03 15:37:52 +0545

    hashed the password

--- src/secret/command/create-secret/create-secret.service.ts
+++ src/secret/command/create-secret/create-secret.service.ts
@@ -1,12 +1,14 @@
-import { CreateSecret } from "./create-secret";
-import { db } from "database";
+import { CreateSecret } from './create-secret';
+import { db } from 'database';
+import { bcryptService } from 'common';
 
 export class CreateSecretService {
-  public async execute(data: CreateSecret): Promise<void> {
-    await db('secrets').insert({
-      id: data.id,
-      body: data.body,
-      expiresIn: data.expiresIn,
-    });
-  }
+	public async execute(data: CreateSecret): Promise<void> {
+		await db('secrets').insert({
+			id: data.id,
+			body: data.body,
+			password: await bcryptService.hash(data.password),
+			expiresIn: data.expiresIn,
+		});
+	}
 }
--- src/secret/command/create-secret/create-secret.ts
+++ src/secret/command/create-secret/create-secret.ts
@@ -1,10 +1,10 @@
 export class CreateSecret {
-  public readonly id: string;
-  public readonly body: string;
-  public readonly password: string | null;
-  public readonly expiresIn: string;
+	public readonly id: string;
+	public readonly body: string;
+	public readonly password: string;
+	public readonly expiresIn: string;
 
-  constructor(props: CreateSecret) {
-    Object.assign(this, props);
-  }
+	constructor(props: CreateSecret) {
+		Object.assign(this, props);
+	}
 }
